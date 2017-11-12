c07d7296130bfef80e4300d4aee5b4b2627df1ad
ZydrunasG <zydrunas.gaidukovas@gmail.com>
2017-11-12 10:47:51 +0200

    New branch

---
+++ admin/userdelete.php
@@ -0,0 +1,26 @@
+<?php
+$title = "Delete user";
+require_once('includes/config.php');
+
+require('layout/header.php');
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+<?php
+//include header template
+require('layout/footer.php');
+?>
+
